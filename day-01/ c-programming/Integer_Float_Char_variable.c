ed27225403c5e404de8d5c010874d6670ad33508
Mahedi Islam Nadim <hhnadim50@gmail.com>
2026-03-04 19:50:44 +0600

    Create Integer_Float_Char_variable.c

---
+++ day-01/ c-programming/Integer_Float_Char_variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int age = 18; //int type date type 
+    float gpa = 2.78; // float date type
+    char name[10] = "Nadim"; // char data type 
+
+    printf("----------------------OUTPUT---------------------\n");
+    printf("Your age %d.\n",age);
+    printf("your gpa %.2f.\n",gpa);
+    printf("Your name %s.\n",name);
+    return 0;
+}
