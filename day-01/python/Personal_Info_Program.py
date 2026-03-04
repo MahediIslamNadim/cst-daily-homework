c8874ddab14993c354e3acf34416a3df662b54f1
Mahedi Islam Nadim <hhnadim50@gmail.com>
2026-03-04 18:46:04 +0600

    Create Personal_Info_Program.py

---
+++ day-01/python/Personal_Info_Program.py
@@ -0,0 +1,8 @@
+name = input("Enter Your Name : ")
+age = input("Enter Your Age : ")
+address = input("Enter Your address : ")
+
+print("---------------------personal info program------------------------")
+print("your Name is ",name)
+print("your age ", age)
+print("your address ",address)
