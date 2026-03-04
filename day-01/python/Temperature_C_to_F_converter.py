6ce7f9c90a435e1e6db8afd7ad421b65f9016dd6
Mahedi Islam Nadim <hhnadim50@gmail.com>
2026-03-04 18:47:02 +0600

    Create Temperature_C_to_F_converter.py

---
+++ day-01/python/Temperature_C_to_F_converter.py
@@ -0,0 +1,9 @@
+#  F=(C*(9/5))+32
+# input celsius 
+celsius = float(input("Enter you Temperature °C :"))
+
+# Fahrenheit formola
+fahrenheit = (celsius * (9/5) + 32 )
+
+#out put fahrenheit 
+print(f"{celsius}°C = {fahrenheit}°F")
