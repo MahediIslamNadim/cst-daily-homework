2d9ce83372f433e512f77ea7739af114cbdc409a
Mahedi Islam Nadim <hhnadim50@gmail.com>
2026-03-04 18:44:59 +0600

    Create Calculator.py

---
+++ day-01/python/Calculator.py
@@ -0,0 +1,12 @@
+number1= float(input("Enter fast number :"))
+number2 = float(input("Enter second number :"))
+
+addition = number1 + number2
+subtraction = number1 - number2
+Multiplication = number1 * number2
+Divition = number1 / number2 if number2 != 0 else "Error! Divition by zero."
+print("\n ------------------OUTPOT------------------------")
+print("Addition is : ",addition)
+print("Subtraction is : ",subtraction)
+print("Multiplication is : ",Multiplication)
+print("Divition is : ",Divition)
